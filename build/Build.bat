40d167e9fc9b00aeb9b2d8a90ffb421fc4ccfb2d
Alex Humphrey <alexejhumphrey@hotmail.com>
2012-04-20 15:43:12 +0100

    Build script now accepts a parameter specifying the version of .NET to target.
    Targetting .NET 3.5 produces an assembly that is friendlier with
    Powershell.

--- build/Build.bat
+++ build/Build.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /D %~dp0
 set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
-if [%1]==[] (set config=Debug) else (set config=%1)
-if [%2]==[] (set platform=Any CPU) else (set platform=%2)
-%msbuild% ..\Automation\Automation.sln /t:Rebuild /p:Configuration=%config%;Platform="%platform%"
+if [%1]==[] (set version=v4.0) else (set version=%1)
+if [%2]==[] (set config=Debug) else (set config=%2)
+if [%3]==[] (set platform=Any CPU) else (set platform=%3)
+%msbuild% ..\Automation\Automation.sln /t:Rebuild /p:Configuration=%config%;Platform="%platform%";TargetFrameworkVersion=%version%
 exit /b %%errorlevel%%
